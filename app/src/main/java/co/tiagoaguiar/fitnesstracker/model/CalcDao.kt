23e1070c199995390e9ff7eaf1a1546069cd33fa
Renan <euannebeatrizcm@gmail.com>
2023-04-05 17:01:12 -0300

    App saúde

---
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/model/CalcDao.kt
@@ -0,0 +1,14 @@
+package co.tiagoaguiar.fitnesstracker.model
+
+import androidx.room.*
+
+@Dao
+interface CalcDao {
+
+    @Insert
+    fun insert(calc: Calc)
+
+    @Query("SELECT * FROM Calc WHERE type = :type")
+    fun getRegisterByType(type: String) : List<Calc>
+
+}
